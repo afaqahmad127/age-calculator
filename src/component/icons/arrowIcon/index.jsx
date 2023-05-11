95c6dfb9e3d3b15c06d7edb9975b6ab8e7eb033b
Afaq Ahmad <you@example.com>
2023-05-11 17:06:26 +0500

    icons added

---
+++ src/component/icons/arrowIcon/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const ArrowIcon = ({ size }) => {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			width={size}
+			height={size}
+			viewBox="0 0 46 44"
+		>
+			<path
+				fill="none"
+				stroke="#FFF"
+				strokeWidth="2"
+				d="M1 22.019C8.333 21.686 23 25.616 23 44m0 0V0m22 22.019C37.667 21.686 23 25.616 23 44"
+			></path>
+		</svg>
+	);
+};
